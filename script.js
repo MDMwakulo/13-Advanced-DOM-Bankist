9062ddd28dcc6616f074c90bd21bac7c4cfef419
MDMwakulo <mdmwakulo@gmail.com>
2024-03-06 15:06:46 +0300

    Selecting, Creating, and Deleting Elements

--- script.js
+++ script.js
@@ -29,3 +29,44 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// //////////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////////
+
+// Selecting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSection = document.querySelectorAll('.section');
+console.log(allSection);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = `We use cookies for improved functionality and analytics.`;
+message.innerHTML = `We use cookies for improved functionality and analytics. <button class='btn btn--close-cookie'>Got it!</button>`;
+// header.prepend(message); // inserts node before the first child of node
+header.append(message); // inserts node after the last child of node
+// header.append(message.cloneNode(true)); //we pass 'true' so as to copy all the child elements
+
+// header.before(message); // will attach before the header element (like a sibling to the header)
+// header.after(message); // will attach after the header element (like a sibling to the header)
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // message.remove();// remove() is a modern method
+    message.parentElement.removeChild(message);
+  });
