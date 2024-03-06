7b5db4f7a082ca6b4aa3486d216fb4a654a888a2
MDMwakulo <mdmwakulo@gmail.com>
2024-03-06 17:14:52 +0300

    Styles, Attributes and Classes

--- script.js
+++ script.js
@@ -70,3 +70,50 @@ document
     // message.remove();// remove() is a modern method
     message.parentElement.removeChild(message);
   });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor); // rgb(55, 56, 61)
+
+console.log(getComputedStyle(message).color); // rgb(187, 187, 187)
+console.log(getComputedStyle(message).height); // 48.9844px
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className); // nav__logo
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer')); // Durell
+logo.setAttribute('company', 'Bankist'); // opposite of getAttribute
+
+console.log(logo.src); // http://127.0.0.1:8080/img/logo.png
+console.log(logo.getAttribute('src')); // img/logo.png
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href); // http://127.0.0.1:8080/#
+console.log(link.getAttribute('href')); // #
+
+// Data attribute
+console.log(logo.dataset.versionNumber); // 3.0
+
+// Classes
+logo.classList.add(`c`, 'j');
+logo.classList.remove(`c`, 'j');
+logo.classList.toggle(`c`);
+logo.classList.contains(`c`); // contains but not includes
+
+// Do not use this as it would override all the existing classes
+// it will also allow us to only put one class on any element
+logo.className = 'Durell';
